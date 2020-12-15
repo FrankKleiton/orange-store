3dc8e7be44bd5a84b3ab37249f27ce741c01d23c
Franklyn Medeiros <franklynkleiton18@gmail.com>
2020-12-15 11:10:05 -0300

    Adding banner.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Home</h1>
+  <div class="content">
+    <img src="../assets/images/banner.png" />
+  </div>
 </template>
+<script>
+</script>
+<style lang="scss" scoped>
+
+img {
+  width: 100%;
+}
+
+.content {
+  width: 100%;
+}
+
+</style>
